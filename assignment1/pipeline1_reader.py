533fd271ac45f1f70c7db6780c21aaa1ca5f3ea1
ZexinPeng <zexinpeng97@outlook.com>
2021-10-01 16:52:27 -0400

    Initial commit

---
+++ assignment1/pipeline1_reader.py
@@ -0,0 +1,57 @@
+import sys
+import os
+import re
+from bs4 import BeautifulSoup
+
+path = './reuters21578'
+articles_num = 5
+documents = []
+
+def read_from_file():
+    for file in os.listdir(path):
+        # find all files ending with .sgm in the folder
+        if file.endswith('.sgm'):
+            # use latin-1 to decode some characters excluded in utf-8 or gbk
+            with open(os.path.join(path, file), 'r', encoding='latin-1') as f:
+                reuters_file_content = f.read()
+                yield reuters_file_content
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('please input the number of files that should be extracted as parameters. \'python ./pipeline1_reader.py '
+              + '[number of files]\'')
+        sys.exit()
+    articles_num = int(sys.argv[1])
+    print('the number of extracted files is ' + str(articles_num))
+
+    files = read_from_file()
+    # iterate all .sgm files
+    counts = 0
+    for file in files:
+        # get single document
+        for document in re.findall("<REUTERS TOPICS.*?</REUTERS>", file.replace('\n', '')):
+            # use BeautifulSoup to parse the document
+            document_after_parsed = BeautifulSoup(document, 'html.parser')
+
+            if document_after_parsed.find('body') is not None:
+                reuters = document_after_parsed.find('reuters')
+                id = int(reuters.get('newid'))
+                title = document_after_parsed.find('title').string
+                body = document_after_parsed.find('body').string
+                content = title + ' ' + body
+                content_dict = {'documentId': id, 'content': content}
+                documents.append(str(content_dict))
+                counts += 1
+            if counts == articles_num:
+                print(documents)
+                output_dir = "./output/"
+                if not os.path.exists(output_dir):
+                    os.makedirs(output_dir)
+                with open(output_dir + 'pipeline1.txt', "w") as f:
+                    for line in documents:
+                        f.write(line + '\n')
+                sys.exit()
+
+
+
